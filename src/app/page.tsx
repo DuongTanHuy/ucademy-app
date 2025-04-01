b11e8f0e72cded484e875ac8399fa59920e73e9f
DuongTanHuy <huy236236@gmail.com>
2025-04-01 11:32:43 +0700

    Next font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1 className="font-sf_mono">Hello World</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eaque
+        ipsa incidunt nihil vero culpa sint excepturi, cumque dolore corrupti
+        possimus odit porro earum tempore quis, ipsum natus? Vel, animi.
+      </p>
     </main>
   );
 }
---
+++ src/components/fonts.ts
@@ -0,0 +1,35 @@
+import { Manrope, Roboto } from "next/font/google";
+import localFont from "next/font/local";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-manrope",
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto",
+});
+
+const sf_mono = localFont({
+  src: [
+    {
+      path: "./local-fonts/SFMono-Regular.otf",
+      weight: "400",
+    },
+    {
+      path: "./local-fonts/SFMono-Medium.otf",
+      weight: "500",
+    },
+    {
+      path: "./local-fonts/SFMono-Bold.otf",
+      weight: "700",
+    },
+  ],
+  variable: "--font-sf-mono",
+  display: "swap",
+});
+
+export { manrope, roboto, sf_mono };
