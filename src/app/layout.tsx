18cf336232bb8fc4c7b813638000d394c8143101
DuongTanHuy <huy236236@gmail.com>
2025-04-01 10:09:44 +0700

    init project nextjs 14

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ucademy",
+  description: "Nền tảng học trực tuyến siêu chất lượng",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={manrope.className}>{children}</body>
+    </html>
+  );
+}
