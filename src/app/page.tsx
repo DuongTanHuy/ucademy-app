adf7d6f27a520c58ca3073d5e452b68c0b3f3f44
DuongTanHuy <huy236236@gmail.com>
2025-04-01 11:32:43 +0700

    Next font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam eaque
+        ipsa incidunt nihil vero culpa sint excepturi, cumque dolore corrupti
+        possimus odit porro earum tempore quis, ipsum natus? Vel, animi.
+      </p>
     </main>
   );
 }
